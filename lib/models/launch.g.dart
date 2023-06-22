35e2d1038209c4d52975252af70894704dc569b0
Asim Mahmood <asim1999@hotmail.co.uk>
2023-06-22 19:15:31 +0100

    Made launchSuccess field nullable to fix loading error

--- lib/models/launch.g.dart
+++ lib/models/launch.g.dart
@@ -10,12 +10,12 @@ Launch _$LaunchFromJson(Map<String, dynamic> json) => Launch(
       siteId: json['launch_site']['site_id'] as String,
       missionName: json['mission_name'] as String,
       launchDate: Launch._fromJson(json['launch_date_unix'] as int),
-      launchSuccess: json['launch_success'] as bool,
+      launchSuccess: json['launch_success'] as bool?,
       details: json['details'] as String?,
     );
 
 Map<String, dynamic> _$LaunchToJson(Launch instance) => <String, dynamic>{
-      'launch_site.site-id': instance.siteId,
+      'launch_site.site_id': instance.siteId,
       'mission_name': instance.missionName,
       'launch_date_unix': Launch._toJson(instance.launchDate),
       'launch_success': instance.launchSuccess,

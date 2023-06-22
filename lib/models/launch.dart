6e17a68ea032143f7afce1ce202460aa167b697c
Asim Mahmood <asim1999@hotmail.co.uk>
2023-06-22 17:47:26 +0100

    Added launch.dart and launchpad.dart classes

---
+++ lib/models/launch.dart
@@ -0,0 +1,46 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'launch.g.dart';
+
+@JsonSerializable()
+class Launch {
+  @JsonKey(name: 'launch_site.site-id')
+  final String siteId;
+
+  @JsonKey(name: 'mission_name')
+  final String missionName;
+
+  @JsonKey(
+    name: 'launch_date_unix',
+    toJson: _toJson,
+    fromJson: _fromJson,
+  )
+  final DateTime launchDate;
+
+  @JsonKey(name: 'launch_success')
+  final bool launchSuccess;
+
+  @JsonKey(name: 'details')
+  final String details;
+
+  Launch({
+    required this.siteId,
+    required this.missionName,
+    required this.launchDate,
+    required this.launchSuccess,
+    required this.details,
+  });
+
+  /// Connect the generated [_$LaunchFromJson] function to the `fromJson`
+  /// factory.
+  factory Launch.fromJson(Map<String, dynamic> json) => _$LaunchFromJson(json);
+
+  /// Connect the generated [__$LaunchToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$LaunchToJson(this);
+
+  //From and toJson date methods
+  static int _toJson(DateTime value) => value.millisecondsSinceEpoch;
+
+  static DateTime _fromJson(int value) =>
+      DateTime.fromMillisecondsSinceEpoch(value);
+}
---
+++ lib/models/launchpad.dart
@@ -0,0 +1,38 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'launchpad.g.dart';
+
+@JsonSerializable()
+class LaunchPad {
+
+  @JsonKey(name: 'site_id')
+  final String siteId;
+
+  @JsonKey(name: 'location.longitude')
+  final double long;
+
+  @JsonKey(name: 'location.latitude')
+  final double lat;
+
+  @JsonKey(name: 'location.name')
+  String name;
+
+  @JsonKey(name: 'location.region')
+  String region
+
+  @JsonKey(name: 'details')
+  String details;
+
+
+  LaunchPad(
+      {required this.siteId, required this.long, required this.lat, this.name, this.region,
+        required this.details,});
+
+  /// Connect the generated [_$LaunchFromJson] function to the `fromJson`
+  /// factory.
+  factory Launchpad.fromJson(Map<String, dynamic> json) =>
+      _$LaunchpadFromJson(json);
+
+  /// Connect the generated [__$LaunchpadToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$LaunchpadToJson(this);
+}

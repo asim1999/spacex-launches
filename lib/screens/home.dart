0badeb352b0e9c1dc3707ba549d1014517b3b55c
Asim Mahmood <asim1999@hotmail.co.uk>
2023-06-22 18:35:01 +0100

    Added launchpad_expansion_card.dart and toggle in home.dart to show details

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:space_x_launches/widgets/home/launchpad_expansion_card.dart';
 
 import '../models/launchpad.dart';
 
@@ -13,15 +15,20 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //Controller for controlling expanded and collapsed state
+  late ExpandableController _expandableController;
+
   //Late variable to track getting of launchpads
   late Future<List<Launchpad>> _launchpads;
 
   Future<List<Launchpad>> _getLaunchPads() async {
-    final response = await http.get(Uri.parse('https://api.spacexdata.com/v3/launchpads'));
+    final response =
+        await http.get(Uri.parse('https://api.spacexdata.com/v3/launchpads'));
     List jsonData = jsonDecode(response.body);
-    print(jsonDecode(response.body).runtimeType);
-  //  Convert to objects
-    List<Launchpad> launchpads = jsonData.map((e) => Launchpad.fromJson(e as Map<String,dynamic>)).toList();
+    //  Convert to objects
+    List<Launchpad> launchpads = jsonData
+        .map((e) => Launchpad.fromJson(e as Map<String, dynamic>))
+        .toList();
     return launchpads;
   }
 
@@ -29,28 +36,46 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
+    //Set the controller
+    _expandableController = ExpandableController();
+    //Start the getting of launchpads
     _launchpads = _getLaunchPads();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Launchpads'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () => _expandableController.toggle(),
+            icon: const Icon(
+              Icons.menu,
+            ),
+          ),
+        ],
       ),
       body: FutureBuilder<List<Launchpad>>(
         future: _launchpads,
         builder: (context, snapshot) {
-          if(!snapshot.hasData){
+          if (!snapshot.hasData) {
             return const Center(
               child: CircularProgressIndicator(),
             );
           }
           //Get the launchpads
           final launchpads = snapshot.data!;
-          return Center(
-            child: Text('${launchpads.length}'),
+          return ListView.builder(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            itemCount: launchpads.length,
+            itemBuilder: (context, index) => LaunchpadExpansionCard(
+              launchpad: launchpads[index],
+              controller: _expandableController,
+            ),
           );
         },
       ),
